9a07b4c633dd8efa6242350cb8b0db6ec278a99b
Lookis <lookisliu@gmail.com>
2020-01-31 00:27:57 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -17,7 +17,7 @@ if (process.env.BROWSER) {
 
 module.exports = {
   // Node.js app
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 3002,
 
   baseUrl: process.env.BASE_URL || '',
 
@@ -28,7 +28,7 @@ module.exports = {
     // API URL to be used in the server-side code
     serverUrl:
       process.env.API_SERVER_URL ||
-      `http://localhost:${process.env.PORT || 3000}`,
+      `http://localhost:${process.env.PORT || 3002}`,
   },
 
   // Database
